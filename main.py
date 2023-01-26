96989ed2765c367482afef78a70165e2930bb005
Devin Hopkins <devinhopkins@gmail.com>
2023-01-26 11:14:13 -0500

    .

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ st.title('ED Discharge Assistant')
 
 if params:
     # st.sidebar.write(params)
+    st.sidebar.write(params['unumber'][0])
     st.sidebar.write(params['name'][0])
     st.sidebar.write(params['ramq'][0])
+    st.sidebar.write(params['dob'][0])
+    st.sidebar.write(params['phone'][0])
+    st.sidebar.write(params['postalcode'][0])
